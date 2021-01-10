758d3246d154d868f53f2377e61e510775218b33
Matthew Via <via@matthewvia.info>
2021-01-10 00:31:03 -0500

    initial table editing

--- src/TableEditor.cxx
+++ src/TableEditor.cxx
@@ -3,11 +3,68 @@
 
 #include <FL/fl_draw.H>
 
+void TableEditor::cell_select_callback(Fl_Widget *w, void *ptr) {
+  auto editor = static_cast<TableEditor *>(w);
+  int R = editor->callback_row();
+  int C = editor->callback_col();
+
+  switch (editor->callback_context()) {
+    case CONTEXT_TABLE:
+    case CONTEXT_ROW_HEADER:
+    case CONTEXT_COL_HEADER:
+      editor->stop_editor();
+      break;
+    case CONTEXT_CELL:
+      switch (Fl::event()) {
+        case FL_PUSH:
+          editor->stop_editor();
+          editor->start_editor(R, C);
+          break;
+      }
+    default:
+      break;
+  }
+}
+
+void TableEditor::cell_value_callback(Fl_Widget *w, void *ptr) {
+  auto editor = static_cast<TableEditor *>(ptr);
+  std::cerr << "set value: " << editor->input->value() << std::endl;
+  editor->stop_editor();
+}
+
+void TableEditor::stop_editor() {
+  input->hide();
+  window()->cursor(FL_CURSOR_DEFAULT);
+  editing = false;
+}
+
+void TableEditor::start_editor(int r, int c) {
+  set_selection(r, c, r, c);
+  
+  int X, Y, W, H;
+  find_cell(CONTEXT_CELL, r, c, X, Y, W, H);
+  input->resize(X, Y, W, H);
+  auto v = cell_value(r, c);
+  input->value(v.c_str());
+  input->position(0, v.size());
+  input->show();
+  input->take_focus();
+  edit_r = r;
+  edit_c = c;
+  editing = true;
+}
+
 TableEditor::TableEditor(int X, int Y, int W, int H) : Fl_Table(X, Y, W, H) {
   row_header(1);
   col_header(0);
   rows(0);
   cols(0);
+  callback(cell_select_callback);
+
+  input = new Fl_Float_Input(0, 0, 0, 0);
+  input->callback(cell_value_callback, this);
+  input->when(FL_WHEN_ENTER_KEY_ALWAYS);
+  input->hide();
   end();
 }
 
@@ -24,6 +81,16 @@ void TableEditor::setTable(viaems::TableValue t) {
   }
   col_width_all(40);
   row_height_all(25);
+}
+
+std::string TableEditor::cell_value(int r, int c) {
+  char buf[32];
+  if (this->table.axis.size() == 2) {
+    snprintf(buf, sizeof(buf), "%.3g", this->table.two[r][c]);
+  } else {
+    snprintf(buf, sizeof(buf), "%.3g", this->table.one[r]);
+  }
+  return std::string{buf};
 }
 
 void TableEditor::draw_cell(TableContext c, int R, int C, int X, int Y, int W,
@@ -51,17 +118,13 @@ void TableEditor::draw_cell(TableContext c, int R, int C, int X, int Y, int W,
     fl_pop_clip();
     break;
   case CONTEXT_CELL: {
+    if (editing && (R == edit_r) && (C == edit_c)) return;
     fl_draw_box(FL_THIN_UP_BOX, X, Y, W, H, FL_WHITE);
     fl_color(FL_BLACK);
     fl_push_clip(X, Y, W, H);
 
-    if (this->table.axis.size() == 2) {
-      snprintf(buf, sizeof(buf), "%.3g", this->table.two[R][C]);
-    } else {
-      snprintf(buf, sizeof(buf), "%.3g", this->table.one[R]);
-    }
     buf[31] = '\0';
-    fl_draw(buf, X + 1, Y, W, H, FL_ALIGN_CENTER);
+    fl_draw(cell_value(R, C).c_str(), X + 1, Y, W, H, FL_ALIGN_CENTER);
     fl_pop_clip();
   } break;
   case CONTEXT_RC_RESIZE: {
--- src/TableEditor.h
+++ src/TableEditor.h
@@ -2,6 +2,7 @@
 
 #include <FL/Fl.H>
 #include <FL/Fl_Table.H>
+#include <FL/Fl_Float_Input.H>
 
 #include "viaems.h"
 
@@ -12,6 +13,17 @@ public:
   void setTable(viaems::TableValue table);
 
 private:
+  Fl_Float_Input *input;
   viaems::TableValue table;
+
   void draw_cell(TableContext c, int R, int C, int X, int Y, int W, int H);
+  std::string cell_value(int r, int c);
+
+  static void cell_select_callback(Fl_Widget *w, void *);
+  static void cell_value_callback(Fl_Widget *w, void *);
+  void start_editor(int R, int C);
+  void stop_editor();
+
+  bool editing = false;
+  int edit_r, edit_c;
 };
