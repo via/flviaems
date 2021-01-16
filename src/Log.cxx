1ef496ab6a9fe7f904d4e7534164e9e59a827014
Matthew Via <via@matthewvia.info>
2021-01-16 18:11:08 -0500

    meh

--- src/Log.cxx
+++ src/Log.cxx
@@ -1,11 +1,40 @@
+
 #include "Log.h"
 
 Log::Log() {}
 
-void Log::Update(std::vector<viaems::FeedUpdate> list) {
-#if 0
-  for (auto &entry : list) {
-    this->log.push_back(entry);
+void Log::ensure_section_usable() {
+  if (sections.empty()) {
+    sections.push_back(LogSection{.ready = true});
+  } else if (sections[sections.size() - 1].full()) {
+    auto& section = sections[sections.size() - 1];
+    uint64_t pos = 0;
+    if (section.dirty) {
+      /* Write to file, update section_length, free memory */
+      section.points.clear();
+    }
+    sections.push_back(LogSection{
+      .file_position = pos,
+      .ready = true,
+    });
   }
-#endif
+}
+
+void Log::Update(std::vector<viaems::FeedUpdate> list) {
+  ensure_section_usable();
+
+  auto& section = sections[sections.size() - 1];
+  for (auto &entry : list) {
+    LogPoint point{};
+    point.time = entry.time;
+    for (auto &x : entry) {
+      point.values.push_back(x.second);
+    }
+    section.points.push_back(point);
+    section.dirty = true;
+  }
+}
+
+void Log::SetOutputFile(std::string path) {
+  logfile.open(path);
 }
--- src/Log.h
+++ src/Log.h
@@ -1,12 +1,33 @@
 #include <chrono>
 #include <deque>
 #include <vector>
+#include <fstream>
 
 #include "viaems.h"
 
+struct LogPoint {
+  viaems::FeedTime time;
+  std::vector<viaems::FeedValue> values;
+};
+
+struct LogSection {
+  uint64_t file_position;
+  uint64_t section_length;
+
+  bool ready;
+  bool dirty;
+
+  std::vector<std::string> keys;
+
+  std::vector<LogPoint> points;
+  bool full() {return points.size() > 50000;};
+};
+
 class Log {
   std::fstream logfile;
-  std::deque<viaems::FeedUpdate> log;
+  std::vector<LogSection> sections;
+
+  void ensure_section_usable();
 
 public:
   Log();
@@ -14,7 +35,7 @@ public:
   void SetOutputFile(std::string path);
   void Update(std::vector<viaems::FeedUpdate>);
 
-  std::vector<viaems::FeedUpdate>
+  std::vector<LogPoint>
   GetRange(std::chrono::system_clock::time_point start,
-           std::chrono::system_clock::time_point end);
+      std::chrono::system_clock::time_point end);
 };
