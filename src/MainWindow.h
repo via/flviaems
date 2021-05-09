e1a02f0c8bb78b9e435159f793bc8a70d66f3b82
via <via@zenbox.inside.matthewvia.info>
2021-05-09 10:13:06 -0400

    Move log handling to toplevel flviaems, save logs on interrogation

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -7,9 +7,8 @@
 
 class MainWindow : public MainWindowUI {
   viaems::Model *m_model;
-  Log log_reader;
-  ThreadedWriteLog log_writer;
   viaems::StructurePath detail_path;
+  std::optional<Log *> log;
 
   void update_config_structure(viaems::StructureNode top);
 
@@ -23,13 +22,14 @@ class MainWindow : public MainWindowUI {
 
 public:
   MainWindow();
-  void feed_update(viaems::LogChunk &&);
+  void feed_update(std::map<std::string, viaems::FeedValue> status);
   void update_connection_status(bool status);
   void update_feed_hz(int hz);
   void update_model(viaems::Model *model);
   void update_interrogation(bool in_progress, int value, int max);
   void update_config_value(viaems::StructurePath path,
                            viaems::ConfigValue value);
+  void update_log(std::optional<Log *>);
 };
 
 #endif
