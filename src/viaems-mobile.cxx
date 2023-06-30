c3de54fc98fe5e9dc6a48da2aadadb2e718b9ee3
Matthew Via <via@matthewvia.info>
2023-06-29 21:15:09 -0400

    make mobile have a statustable

--- src/viaems-mobile.cxx
+++ src/viaems-mobile.cxx
@@ -3,7 +3,9 @@
 #include <FL/Fl_Dial.H>
 
 #include "viaems.h"
+#include "StatusTable.h"
 
+static StatusTable *bleh = nullptr;
 static void updates(void *_ptr) {
   viaems::Protocol* proto = (viaems::Protocol *)_ptr;
   proto->NewData();
@@ -13,6 +15,14 @@ static void updates(void *_ptr) {
     std::cerr << key << " ";
   }
   std::cerr << ")" << std::endl;
+
+  std::map<std::string, viaems::FeedValue> status;
+  for (unsigned int i = 0; i < chunk.keys.size(); i++) {
+    status.insert(
+        std::make_pair(chunk.keys[i], chunk.points[0].values[i]));
+  }
+
+  bleh->feed_update(status);
   Fl::repeat_timeout(0.05, updates, _ptr);
 }
 
@@ -22,12 +32,14 @@ int main(int argc, char **argv) {
    std::shared_ptr<viaems::Protocol> protocol;
 
   Fl_Window *window = new Fl_Window(340, 180);
-  Fl_Dial *dial = new Fl_Dial(0, 0, 100, 100, "RPM");
+  StatusTable *status = new StatusTable(0, 0, 340, 180, "Status");
+  bleh = status;
   window->end();
   window->show(argc, argv);
   std::unique_ptr<viaems::UsbConnection> conn = std::make_unique<viaems::UsbConnection>();
   std::cerr << "connect: " << conn->Connect() << std::endl;
   protocol = std::make_shared<viaems::Protocol>(std::move(conn));
+
   Fl::add_timeout(0.05, updates, &*protocol);
   return Fl::run();
 }
